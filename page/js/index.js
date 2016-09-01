53279212c75d8b3946e7fa09386a7727e00f7c72
Ephim64 <Gotsco-en@yandex.ru>
2016-09-01 11:25:25 +0400

    most of js features done ex modal wind

--- page/js/index.js
+++ page/js/index.js
@@ -1,72 +1,84 @@
+var table,
+merchandise = [],
+tempItem = {};
+//EVENT BINDING
 $(function(){
-//VARIABLES
-  var table = $('#list tbody').clone();
-  var merchandise = [];
-  var action = true;//fals = update true = add
-  var purgItem = {};
-//EVENTS
-  $('#add-update').click(function(e){
+  $('#add-update').click(function(event) {
     var item = {'name':$('#merch-name').val(),'count':$('#count').val(),'price':$('#price').val()};
-    if(item.name && item.price && item.count){
-      if(action) addItem(item);
-      else updateItem(item,purgItem);
-    }
+    if(item.name && item.count && item.price){
+    addItem(item);
+    appendToTable(item);
+    toInput();
+  }
   });
-  $('#list').on('click','button',function(e){
-    console.log($(this).attr('value'));
-    if($(this).attr('value')==='edit'){
-      toInput(merchandise[$(this).attr('data-index')]);
+  $('#list').on('click','button',function(){
+    if($(this).attr('value') === 'edit'){
+      removeItem($(this).attr('data-index'));
+      updateTable();
     }
-    else if($(this).attr('value')==='delete'){
+    if($(this).attr('value') === 'delete'&&confirm('Are you willing to remove this item?')){
       removeItem($(this).attr('data-index'));
       updateTable();
     }
   });
-  $('#price').on('focusout',function(){
-    console.log('focus out');
-    $('price').val('$ '+$('price').attr('data-price').replace(/(\d)(?=(\d{3})+\.)/g, '$&,'))
-  })
-//FUNCTIONS
-  function addItem(item){
-    merchandise.push(item);
-    appentToTable(item);
-    toInput();
-  }
-  function updateItem(item,newItem){
-    console.log('update');
-  }
-  function removeItem(index){
-    console.log(merchandise.length === 1);
-    if(merchandise.length === 1) merchandise = [];
-    else{
-      merchandise = merchandise.filter(function(element){
-        return merchandise.indexOf(element) != index;
-      })
-    }
-  }
-  function appentToTable(item){
-    var print = '<td>'+item.name+'</td>'+'<td>'+item.count+'</td>'+'<td>'+item.price+'</td>';
-    print += '<td><button value="edit" data-index="'+merchandise.indexOf(item)+'">Edit</button>'
-    print += '<button value="delete" data-index="'+merchandise.indexOf(item)+'">Delete</button></td>'
-    $('#list tbody').append('<tr data-index=\''+merchandise.indexOf(item)+'\'>');
-    var row = $('[data-index=\''+merchandise.indexOf(item)+'\']');
-    row.append(print);
-  }
-  function toInput(item){
-    item? $('#merch-name').val(item.name):$('#merch-name').val('');
-    item? $('#price').val(item.price).attr('data-price',item.price):$('#price').val('');
-    item? $('#count').val(item.count):$('#count').val('');
-  }
-  function updateTable(){
-    $('#list tbody').replaceWith(table);
-    if(merchandise) merchandise.forEach(function(element){
-      appentToTable(element);
-    })
-    else $('#list tbody').empty();
-  }
-  $('#add-new').click(function(){
-    var number = 1342568.255;
-    var reg = '(\d)(?=(\d{3})+(?:\.\d+)?&)'
-    console.log('$ ' + number.toString().replace(/(\d)(?=(\d{3})+\.)/g, '$&,'));
-  })
 })
+$(function(){
+  table = $('#list').clone(true);
+})
+//EVENT BINDING ENDS
+//DATA PROCESSING
+function addItem(item){
+  console.log('add');
+  console.log(item.name);
+  console.log(item.count);
+  console.log(item.price);
+  merchandise.push(item);
+}
+function updateItem(index,item){
+  console.log('update');
+  merchandise[index] = item;
+}
+function removeItem(index){
+  console.log('remove');
+  console.log(index);
+  merchandise = merchandise.filter(function(element){
+    return merchandise.indexOf(element) != index;
+  });
+}
+//END DATA PROCESSING
+//VISUAL OPERATIONS
+function toInput(item){
+  item? $('#merch-name').val(item.name):$('#merch-name').val('');
+  item? $('#count').val(item.count):$('#count').val('');
+  item? $('#price').val(item.price):$('#price').val('');
+}
+function appendToTable(item){
+  console.log(merchandise.indexOf(item));
+  var row = '<tr>';
+  row += '<td>'+item.name;
+  row += '<td>'+item.count;
+  row += '<td>'+item.price;
+  row += '<td><button value="edit" data-index="'+merchandise.indexOf(item)+'">Edit</button>';
+  row += '<button value="delete" data-index="'+merchandise.indexOf(item)+'">Delete</button>';
+  $('#list tbody').append(row);
+}
+function updateTable(){
+  console.log('update table');
+  console.log(!merchandise);
+  recreateTable();
+  if(merchandise){
+    merchandise.forEach(function(item){
+      appendToTable(item);
+    })
+  }
+}
+function recreateTable(){
+  var header = '<tr>';
+  header += '<td>Name<span>';
+  header += '<td class="count"><span>'
+  header += '<td>Price<span>';
+  header += '<td>Actions';
+  $('#list tbody').empty();
+  $('#list tbody').append(header);
+}
+//END VISUAL OPERATIONS
